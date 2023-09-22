6efa104fd698e9be48216fd1a218554e9ee82f0e
muhammadharoon0000 <meerharoon0000@gmail.com>
2023-09-22 22:25:27 +0500

    added show article and tested.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "pages#index"
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show];
 end
